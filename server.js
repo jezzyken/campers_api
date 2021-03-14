fdfa6fed27b611aab1272e123d751f82470e2891
jezrelljolampong <jezrelljolampong17@gmail.com>
2021-03-14 22:46:28 +0800

    Setting up routes and db

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 //env config
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
+//routes
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+// mount routes
+app.use("/api/v1/bootcamps/", bootcamps);
 
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1))
+})
